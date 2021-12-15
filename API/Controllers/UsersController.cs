9fc4b056096782503d651c54bff3dbd88e02fee1
patrick <patrickguedes@outlook.com>
2021-12-15 20:51:24 -0300

    Authentication basics - JWT

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,12 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         public DataContext _context;
         public UsersController(DataContext context)
@@ -15,14 +14,15 @@ namespace API.Controllers
             _context = context;
         }
 
+[AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetAllUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
-        [HttpGet]
-        [Route("{id}")]
+        [Authorize]
+        [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
