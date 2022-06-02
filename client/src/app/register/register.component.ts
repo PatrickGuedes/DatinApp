a1f10a0c1b500429fefec5102fb4fd9591b443c6
patrick <patrickguedes@outlook.com>
2022-06-01 21:48:29 -0300

    better way of handling form validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -14,7 +14,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   registerForm: FormGroup;
   maxDate: Date;
-  validationErrors : string[];
+  validationErrors: string[];
 
   constructor(
     private accountService: AccountService,
@@ -31,13 +31,13 @@ export class RegisterComponent implements OnInit {
   initializeForm() {
 
     this.registerForm = this.fb.group({
-      username: ['', Validators.required],
+      username: ['', [Validators.required, this.alphanumericValidator()]],
       gender: ['male'],
       knownAs: ['', Validators.required],
       dateOfBirth: ['', Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(10)]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(8), this.uppercaseValidator(), this.digitValidator()]],
       confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     })
 
@@ -47,9 +47,26 @@ export class RegisterComponent implements OnInit {
   }
 
   matchValues(matchTo: string): ValidatorFn {
-    console.log('control');
     return (control: AbstractControl) => {
       return control?.value === control?.parent?.controls[matchTo].value ? null : { isMatching: true }
+    }
+  }
+
+  uppercaseValidator(): ValidatorFn {
+    return (control: AbstractControl) => {
+      return /[A-Z]/.test(control?.value) === true ? null : { isUppercase: true };
+    }
+  }
+
+  digitValidator(): ValidatorFn {
+    return (control: AbstractControl) => {
+      return /[0-9]/.test(control?.value) === true ? null : { hasDigit: true };
+    }
+  }
+
+  alphanumericValidator(): ValidatorFn {
+    return (control: AbstractControl) => {
+      return /^[a-z0-9]+$/i.test(control?.value) === true ? null : { isAlphanumeric: true };
     }
   }
 
@@ -57,15 +74,12 @@ export class RegisterComponent implements OnInit {
     this.accountService.register(this.registerForm.value).subscribe(response => {
       this.router.navigateByUrl('/members');
       this.cancel();
-    }, error => {
-      this.validationErrors = error.error;
     })
   }
 
   cancel() {
     this.cancelRegister.emit(false);
   }
-
 
 }
 
